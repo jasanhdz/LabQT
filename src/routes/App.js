6f4bb419a87f8b4842ca8d4159a11cb7ccf88d6d
JasanHdz <jasan_azael@hotmail.es>
2019-10-27 00:29:17 -0500

    prueba 0.0.5

--- src/routes/App.js
+++ src/routes/App.js
@@ -5,12 +5,13 @@ import Home from '../containers/Home.jsx';
 import Login from '../containers/Login.jsx';
 import SignUp from '../containers/Register.jsx';
 import NotFound from '../components/NotFound.jsx';
+require('dotenv').config();
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter path={process.env.PUBLIC_URL + '/'}>
     <Layout>
       <Switch>
-        <Route exact path='/' component={Home}/>
+        <Route exact path="/" component={Home}/>
         <Route exact path="/login" component={Login}/>
         <Route exact path="/register" component={SignUp} />
         <Route component={NotFound} />
